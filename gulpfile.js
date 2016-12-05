833f521deb2d843396836727dfb4cda16eab0faf
Eric Pan <ericpanbot@gmail.com>
2016-12-04 16:25:51 -0800

    Gulp watches for file changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,14 @@ var gulp = require('gulp');
 // requires the gulp-sass plugin
 var sass = require('gulp-sass');
 
+gulp.task('watch', function() {
+  gulp.watch('app/scss/**/*.+(sass|scss)', ['sass']);
+});
+
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.+(sass|scss)')
     .pipe(sass({
-      precision: 2 // Sets number of decimal points to 2
+      precision: 4 // Sets number of decimal points to 2
     })) // Compiles Sass into CSS with gulp-sass
     .pipe(gulp.dest('app/css'))
 });
