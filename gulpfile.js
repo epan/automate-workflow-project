9e4424d21d1f1b54bce6a1b60c51f88060c71b3f
Eric Pan <ericpanbot@gmail.com>
2016-12-04 16:14:25 -0800

    Fix indentation

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.+(sass|scss)')
-  .pipe(sass({
-    precision: 2 // Sets number of decimal points to 2
-  })) // Compiles Sass into CSS with gulp-sass
-  .pipe(gulp.dest('app/css'))
+    .pipe(sass({
+      precision: 2 // Sets number of decimal points to 2
+    })) // Compiles Sass into CSS with gulp-sass
+    .pipe(gulp.dest('app/css'))
 });
