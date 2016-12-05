4832ae9c1c5621f50f1d3b99d6646175b0447c8f
Eric Pan <ericpanbot@gmail.com>
2016-12-05 01:04:38 -0800

    Use browserSync to auto refresh on sass change

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,21 @@ var sass = require('gulp-sass');
 // for error prevention for multiple plugins
 var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
+var browserSync = require('browser-sync');
 
-gulp.task('watch', ['sass'], function() {
+gulp.task('watch', ['browserSync', 'sass'], function() {
   gulp.watch('app/scss/**/*.+(sass|scss)', ['sass']);
 });
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.+(sass|scss)')
     .pipe(customPlumber('Error Running Sass'))
-    .pipe(sass({
-      precision: 4 // Sets number of decimal points to 2
-    })) // Compiles Sass into CSS with gulp-sass
+    .pipe(sass()) // Compiles Sass into CSS with gulp-sass
     .pipe(gulp.dest('app/css'))
+    // tells browserSync to reload files once task is done
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 function customPlumber(errTitle) {
@@ -27,3 +30,12 @@ function customPlumber(errTitle) {
     })
   });
 }
+
+// browserSync task use web server
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
