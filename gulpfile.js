bb6df6a66e3abdd4acaaab799684ce4e26e0d059
Eric Pan <ericpanbot@gmail.com>
2016-12-04 16:31:35 -0800

    Catch errors for watcher and log to command line

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,16 @@ gulp.task('watch', function() {
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.+(sass|scss)')
+    .pipe(sass().on('error', errorHandler))
     .pipe(sass({
       precision: 4 // Sets number of decimal points to 2
     })) // Compiles Sass into CSS with gulp-sass
     .pipe(gulp.dest('app/css'))
 });
+
+function errorHandler(err) {
+  // log error in command line
+  console.log(err.toString());
+  // ends the current pipe so Gulp watch doesn't break
+  this.emit('end');
+}
